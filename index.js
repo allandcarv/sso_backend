6ac1f5c535fe4c3b9f8ec944a273c7b6025323d0
Allan Carvalho <allandcarv@gmail.com>
2019-07-03 13:03:33 -0300

    Middlewares

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const consign = require('consign');
-const db = require('../config/db');
+const db = require('./config/db');
 
 app.db = db;
 
 consign()
-    //.then('./config/middlewares.js')
+    .then('./config/middlewares.js')
     //.then('./api')
     .then('./config/routes.js')
     .into(app);
